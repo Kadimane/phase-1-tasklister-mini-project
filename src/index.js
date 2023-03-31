4c2a0d2dd03dc65e6494df1cedee9f478060593c
kadimane <kadimanegloriah@gmail.com>
2023-04-01 00:25:21 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form= document.querySelector("form#create-task-form");
+form.addEventListener("submit", (e) =>{
+  console.log(e);
+  e.preventDefault();
+  console.log(e.target);
+  const value = e.target.querySelector("#new-task-description").value
+  console.log(value);
+  const ul = document.getElementById("tasks");
+  const li = document.createElement("li");
+  console.log(li);
+  li.textContent = value;
+  ul.append(li)
+
+})
